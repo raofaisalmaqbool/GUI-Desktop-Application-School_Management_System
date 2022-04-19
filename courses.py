67c8b8fd936ad46a555b364558146682e56dc78a
raofaisalmaqbool <raofaisalmaqbool@gmail.com>
2022-04-19 07:02:01 +0500

    add title bar in course window

--- courses.py
+++ courses.py
@@ -13,6 +13,13 @@ class CourseCls:
         self.root.config(bg="white")     # color of background
         self.root.focus_force()
 
+        # ==== Title bar ======
+        # set the title bar.. heading... darkblue background         "fg used for font"
+        title_bar = Label(self.root, text="Manage Course Details",font=(
+            "goudy old style", 20, "bold"), bg="#0b5377", fg="white").place(x=10, y=15, width=1180, height=35)
+
+        # ======= Variables ==========
+        
 
        
 
